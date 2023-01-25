dce649c9f5dd6087453e46820e9ccd9f727ad5cf
Mikey Shean <mdshean2@gmail.com>
2023-01-24 17:28:24 -0800

    Add api route for yearly chart

--- django_server/api/serializers.py
+++ django_server/api/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 
-class CpuHoursSerializer(serializers.Serializer):
+class MonthlyCpuHoursSerializer(serializers.Serializer):
     total = serializers.DecimalField(max_digits=10, decimal_places=2)
     month = serializers.DateField()
+
+
+class YearlyCpuHoursSerializer(serializers.Serializer):
+    total = serializers.DecimalField(max_digits=10, decimal_places=2)
+    year = serializers.DateField()
     
--- django_server/api/views.py
+++ django_server/api/views.py
@@ -1,17 +1,27 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django_server.api.serializers import CpuHoursSerializer
+from django_server.api.serializers import MonthlyCpuHoursSerializer, YearlyCpuHoursSerializer
 from django_server.api.services import CpuHoursService
 
 
 class CpuHoursViewSet(ViewSet):
     
-    @action(detail=False, methods=["get"], url_path="daily-hours-by-year")
+    @action(detail=False, methods=["get"], url_path="monthly-hours-by-year")
     def list_monthly_hours_by_year(self, request):
         params = request.query_params
         year = params.get("year")
-        cpu_hours = CpuHoursService.list_monthly_hours_by_year(year)
+        cpu_hours = CpuHoursService.list_monthly_hours_by_year(year=year)
 
-        serializer = CpuHoursSerializer(cpu_hours, many=True)
+        serializer = MonthlyCpuHoursSerializer(cpu_hours, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=False, methods=["get"], url_path="yearly-hours-by-range")
+    def list_yearly_hours_by_range(self, request):
+        params = request.query_params
+        year_start = params.get("year_start")
+        year_end = params.get("year_end")
+        cpu_hours = CpuHoursService.list_yearly_hours_by_range(year_start=year_start, year_end=year_end)
+
+        serializer = YearlyCpuHoursSerializer(cpu_hours, many=True)
         return Response(serializer.data)
